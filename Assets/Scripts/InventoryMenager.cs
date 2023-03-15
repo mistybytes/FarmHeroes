8d821fd8b06736acf2d1e868b4c7044493a699b2
GrzegorzWikar <grzegorzwikar938@gmail.com>
2023-03-15 11:46:10 +0100

    Inventory Items

--- Assets/Scripts/InventoryMenager.cs
+++ Assets/Scripts/InventoryMenager.cs
@@ -11,6 +11,10 @@ public class InventoryMenager : MonoBehaviour
     public Transform ItemContent;
     public GameObject InventoryItem;
 
+    public Toggle EnableRemove;
+
+    public InventoryItemController[] InventoryItems;
+
     private void Awake()
     {
         Instance = this;
@@ -36,11 +40,48 @@ public class InventoryMenager : MonoBehaviour
         foreach (var item in Items)
         {
             GameObject obj = Instantiate(InventoryItem, ItemContent);
-            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
-            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            
+            Text itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            Image itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            Button removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
+
+            if (EnableRemove.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
+        }
+
+        SetInventoryItems();
+
+    }
+
+    public void EnableItemRemove()
+    {
+        if ( EnableRemove.isOn)
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(true);
+            }
+        }else
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for (int i = 0; i < Items.Count; i++)
+        {
+            InventoryItems[i].AddItem(Items[i]);
         }
     }
 }
